b21bdc39026ff77629a3eb035473ce40d74d9cb6
steef96 <steef96.sb@gmail.com>
2020-12-23 15:45:59 +0100

    sidebarUpdate

--- Views/sidemenu.php
+++ Views/sidemenu.php
@@ -1,3 +1,7 @@
-
-
-<h1>hallo</h1>
+<div id="sidenav">
+  <a href="#" class= "bi bi-alarm">Dashboard</a>
+  <a href="#" > <img src="/Pictures/bootstrapIcons/calendar-date.svg" alt="" width="32" height="32" title="Bootstrap"> Agenda</a>
+  <a href="#">Cijfers</a>
+  <a href="#">Toetsen/assessments</a>
+  <a href="#">Administratie</a>
+</div>
