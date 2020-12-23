ccadecc47a6ce29c25aeb764628632d2cf3e051e
Nass1m <70798807+Nass1m@users.noreply.github.com>
2020-12-23 15:42:38 +0100

    HOERENZOOI

--- Views/header.php
+++ Views/header.php
@@ -1,1 +1,7 @@
-<h1>hallo2</h1>
+<nav>
+  <div class="topnav">
+    <a href=#home>Edumatrix</a>
+    <a href=#userProfile class="rightside">mijn profiel</a>
+    <a href=#search class="rightside">zoeken</a>
+  </div>
+</nav>
