8676f74b523ed145b853304fb2f861162eb49695
Nass1m <70798807+Nass1m@users.noreply.github.com>
2020-12-24 18:20:44 +0100

    Lvs 50 (#2)
    * HOERENZOOI
    
    * samenvoegen met sidenav
    
    * header1.0
    
    * Update header.php
    
    * Fixed feedback
    
    Co-authored-by: ExploHash <jimmy@levarne.nl>

--- Views/header.php
+++ Views/header.php
@@ -1,1 +1,5 @@
-<h1>hallo2</h1>
+<nav>
+    <a href="#home" class="left-side"><img src="/images/edulogo_transparent.png" id="edulogo"></a> 
+    <a href="#" class="right-side"><img src="https://icons.getbootstrap.com/icons/power.svg" id="logout"></a>
+    <a href="#" class="right-side"><img src="https://icons.getbootstrap.com/icons/person-square.svg" id="profile"> Mijn profiel</a>
+</nav>
