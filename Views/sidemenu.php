a7c582f38d9853adafa336ec3e2280573fe6e859
steef96 <steef96.sb@gmail.com>
2020-12-24 15:30:06 +0100

    sidemenu 1.0

--- Views/sidemenu.php
+++ Views/sidemenu.php
@@ -1,6 +1,6 @@
 <div id="sidenav">
-  <a href="#" class= "bi bi-alarm"> <img src="Pictures/bootstrapIcons/grid.svg" > Dashboard</a>
-  <a href="#" > <img src="/Pictures/bootstrapIcons/calendar-date.svg"> Agenda</a>
+  <a href="#"><img src="/Pictures/bootstrapIcons/grid.svg"> Dashboard</a>
+  <a href="#"><img src="/Pictures/bootstrapIcons/calendar-date.svg"> Agenda</a>
   <a href="#"><img src="/Pictures/bootstrapIcons/check2-square.svg">Cijfers</a>
   <a href="#"><img src="/Pictures/bootstrapIcons/file-earmark-plus.svg">Toetsen/assessments</a>
   <a href="#"><img src="/Pictures/bootstrapIcons/journals.svg">Administratie</a>
