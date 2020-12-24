ab07fcff0950ed3e2be96b1c7824836427611d38
Nass1m <70798807+Nass1m@users.noreply.github.com>
2020-12-24 17:29:25 +0100

    Update header.php

--- Views/header.php
+++ Views/header.php
@@ -1,4 +1,4 @@
-<nav id="topnav">
+<nav>
   <div class="topnav">
     <a href=#home class="left-side"><img src="/images/edulogo_transparent.png" id="edulogo"></a> 
     <a href=# class="right-side"><img src="https://icons.getbootstrap.com/icons/power.svg" id="logout"></a>
