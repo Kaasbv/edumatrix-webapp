3a47308ea8f087b2f925f201f5b1775b8d869c23
steef96 <steef96.sb@gmail.com>
2020-12-27 22:17:08 +0100

    begin get klas

--- Controllers/SiteController.php
+++ Controllers/SiteController.php
@@ -1,8 +1,12 @@
 <?php
 class SiteController extends Controller {
   public function actionIndex(){
+    $docent = DocentModel::getOne(["id" => 1]);
+    var_dump($docent);
     $this->renderView("main");
+
   }
 }
+
 
 ?>
--- Models/DocentModel.php
+++ Models/DocentModel.php
@@ -1,10 +1,17 @@
 <?php
-class KlasModel extends Model {
-  public static $_tableName = "docentModel";
-  public static $_inheritanceColumn = "_GEBRUIKER_ID";
+class DocentModel extends Model {
+    public static $_tableName = "docentModel";
+    public static $_inheritanceColumn = "_GEBRUIKER_ID";
 
     protected string $code;
     protected int $id;
+
+    public function getKlasDocent($id) {
+        var_dump($this->id);
+        exit();
+        $docentKlassen = KlasModel::getAll(["DOCENT_ID" => $this->id]);
+
+    }
 }
 
 ?>
