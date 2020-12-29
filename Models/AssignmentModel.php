0642a6be48d0e2d99857aef83144ffe887956303
steef96 <steef96.sb@gmail.com>
2020-12-29 16:57:45 +0100

    class beoordeling aangemaakt

---
+++ Models/AssignmentModel.php
@@ -0,0 +1,14 @@
+<?php
+
+class AssignmentModel extends Model {
+    public static $tablename = "Beoordeling";
+
+    protected int $ID;
+    protected string $VAK;
+    protected string $DATUM;
+    protected string $BESCHRIJVING;
+    protected string $DOCENTOPMERKINGEN;
+
+}
+
+?>
