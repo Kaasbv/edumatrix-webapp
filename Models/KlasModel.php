8b566a5e61add9acb7f31552de307c9f33ddd2a2
ExploHash <jbtwaalf@gmail.com>
2021-01-08 22:05:12 +0100

    Forgot

--- Models/KlasModel.php
+++ Models/KlasModel.php
@@ -2,6 +2,13 @@
 class KlasModel extends Model {
   public static $_tableName = "KlasModel";
 
+  protected static $_joins = [
+    [
+      "foreignKey" => "KLAS_ID", //matcht met deze key in koppel tabel bijvoorbeeld klas_id
+      "tableName" => "LeerlingKlas", //koppeltabel
+    ]
+  ];
+
   protected int $id;
   protected string $klasNaam;
   protected int $aantalLeerlingen;
