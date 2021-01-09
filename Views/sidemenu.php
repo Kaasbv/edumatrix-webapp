4a227af47ad3d40943be957772c6fd098879177f
steef96 <steef96.sb@gmail.com>
2021-01-10 00:07:09 +0100

    done (#18)

--- Views/sidemenu.php
+++ Views/sidemenu.php
@@ -1,5 +1,5 @@
-<a href="#"><img src="/Pictures/bootstrapIcons/grid.svg"> Dashboard</a>
+<a href="/"><img src="/Pictures/bootstrapIcons/grid.svg"> Dashboard</a>
 <a href="#"><img src="/Pictures/bootstrapIcons/calendar-date.svg"> Agenda</a>
-<a href="#"><img src="/Pictures/bootstrapIcons/check2-square.svg">Cijfers</a>
+<a href="/cijfer/klassenoverzicht"><img src="/Pictures/bootstrapIcons/check2-square.svg">Cijfers</a>
 <a href="#"><img src="/Pictures/bootstrapIcons/file-earmark-plus.svg">Toetsen/assessments</a>
 <a href="#"><img src="/Pictures/bootstrapIcons/journals.svg">Administratie</a>
