4a64762629f79a04a025da5bb9333767c2d51f9f
ExploHash <jbtwaalf@gmail.com>
2021-01-09 18:57:45 +0100

    Merged

--- Views/Cijfer/sidemenu.php
+++ Views/Cijfer/sidemenu.php
@@ -9,21 +9,18 @@
     <label for="opmerkingen">Opmerkingen:</label>
     <textarea name="opmerkingen" id="opmerkingen"><?= $context->cijfer->opmerkingDocent ?? "" ?></textarea>
     <br>
+    <?php if (isset($_GET["leerlingID"])){ ?>
+        <input type="hidden"  name="leerlingID" value="<?= $_GET["leerlingID"]?>">
+    <?php } ?>
+    
+    <?php if (isset($_GET["beoordelingId"])){  ?>
+        <input type="hidden"  name="beoordelingId" value="<?= $_GET["beoordelingId"]?>">
+    <?php } ?>
+    
+    <?php if (isset($_GET["cijferId"])){ ?>
+        <input type="hidden"  name="cijferId" value="<?= $_GET["cijferId"]?>">
+    <?php } ?>
     <input type="submit" value="Toepassen">
-
-<?php if (isset($_GET["leerlingID"])){ ?>
-    <input type="hidden"  name="leerlingID" value="<?= $_GET["leerlingID"]?>">
-<?php } ?>
-
-<?php if (isset($_GET["beoordelingId"])){  ?>
-    <input type="hidden"  name="beoordelingId" value="<?= $_GET["beoordelingId"]?>">
-<?php } ?>
-
-<?php if (isset($_GET["cijferId"])){ ?>
-    <input type="hidden"  name="cijferId" value="<?= $_GET["cijferId"]?>">
-<?php } ?>
-
-
   </form>
 </div>
 
