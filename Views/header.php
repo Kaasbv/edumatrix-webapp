38114ea821265cf6c424377891c8f061bad756be
steef96 <steef96.sb@gmail.com>
2021-01-10 15:14:12 +0100

    alt img toegevoegd

--- Views/header.php
+++ Views/header.php
@@ -1,3 +1,3 @@
-<a href="/" class="left-side"><img src="/images/edulogo_transparent.png" id="edulogo"></a> 
-<a href="/site/wip" class="right-side"><img src="https://icons.getbootstrap.com/icons/power.svg" id="logout"></a>
-<a href="/site/wip" class="right-side" id ="profileLink"><img src="https://icons.getbootstrap.com/icons/person-square.svg" id="profile"> Mijn profiel</a>
+<a href="/" class="left-side"><img src="/images/edulogo_transparent.png" alt= "Edumatrix logo" id="edulogo"></a> 
+<a href="/site/wip" class="right-side"><img src="https://icons.getbootstrap.com/icons/power.svg" alt= "Logout button" id="logout"></a>
+<a href="/site/wip" class="right-side" id ="profileLink"><img src="https://icons.getbootstrap.com/icons/person-square.svg" alt= "Profiel link" id="profile"> Mijn profiel</a>
