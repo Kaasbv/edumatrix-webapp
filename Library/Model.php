fe9d66cadc569e4e416799be715abe0b64716460
ExploHash <jbtwaalf@gmail.com>
2021-01-10 02:34:02 +0100

    Fixed php 7.4 error

--- Library/Model.php
+++ Library/Model.php
@@ -166,4 +166,12 @@ class Model {
   }
 
 }
+
+//backwards compatability
+if(!function_exists("str_contains")){
+  function str_contains($haystack, $needle){
+    return strpos($haystack, $needle) !== false;
+  }
+}
 ?>
+
