b295195c0811a174a4185a498e49b2745da3b151
steef96 <steef96.sb@gmail.com>
2021-01-10 15:09:08 +0100

    errors fixed

--- Views/header.php
+++ Views/header.php
@@ -1,3 +1,3 @@
-<a href="#home" class="left-side"><img src="/images/edulogo_transparent.png" id="edulogo"></a> 
-<a href="#" class="right-side"><img src="https://icons.getbootstrap.com/icons/power.svg" id="logout"></a>
-<a href="#" class="right-side" id ="profileLink"><img src="https://icons.getbootstrap.com/icons/person-square.svg" id="profile"> Mijn profiel</a>
+<a href="/" class="left-side"><img src="/images/edulogo_transparent.png" id="edulogo"></a> 
+<a href="/site/wip" class="right-side"><img src="https://icons.getbootstrap.com/icons/power.svg" id="logout"></a>
+<a href="/site/wip" class="right-side" id ="profileLink"><img src="https://icons.getbootstrap.com/icons/person-square.svg" id="profile"> Mijn profiel</a>
