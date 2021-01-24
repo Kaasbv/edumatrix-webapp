ee65c77f74aedb9e3274c9793304522b13627330
Nass1m <70798807+Nass1m@users.noreply.github.com>
2021-01-24 16:39:10 +0100

    actionLogin

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -5,8 +5,18 @@
     }
 
     public function actionLogin(){
-      //LVS-113 Hoi nassim
-      $this->renderView("Auth/placeholder_login", ["error" => "poep"]);
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+        $session = GebruikerModel::login($username, $password);
+      } else {
+        $this->renderView("Auth/placeholder_login", ["error" => "Waar is die POST ahhhh zehmer?"]);
+        } if($session) {
+          echo "OMFG IK BEN INGELOGD!!!";
+          //$this->redirect("/dashboard");
+          } else {
+            $this->renderView("Auth/placeholder_login", ["error" => "Wollah dit is geen bestaande user"]);
+          }
     }
 
     public function actionLogout(){
