0347d8491011b1be5a124f1cbc379c3a45aa4e69
MVAHD <marijn@vanaalst.nu>
2021-01-25 22:03:21 +0100

    gg

--- Controllers/RoosterController.php
+++ Controllers/RoosterController.php
@@ -1,8 +1,5 @@
 <?php
     class RoosterController extends Controller{
-        private string $datumVanAf;
-        private string $datumTotMet;
-        
         public function actionAfwezigheidDocent(string $datumVanAf, string $datumTotMet){
             $this->datumVanAf = $datumVanAf;
             $this->datumTotMet = $datumTotMet;
