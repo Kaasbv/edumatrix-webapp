069db18320e05812b3fa3cc9d738df5598787e39
MVAHD <marijn@vanaalst.nu>
2021-01-25 14:57:12 +0100

    Een begin gemaakt aan de RoosterController.

--- Controllers/RoosterController.php
+++ Controllers/RoosterController.php
@@ -1,1 +1,15 @@
-<?php?>
+<?php
+    class RoosterController extends Controller{
+        private string $datumVanAf;
+        private string $datumTotMet;
+        
+        public function actionAfwezigheidDocent(string $datumVanAf, string $datumTotMet){
+            $this->datumVanAf = $datumVanAf;
+            $this->datumTotMet = $datumTotMet;
+
+            $lessen = RoosterDocent->Call();
+            LesDocent::Destroy(int $lessen);
+
+        }
+    }
+?>
