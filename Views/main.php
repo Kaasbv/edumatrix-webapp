78467b06c5a39d23c6e8070507deb14dd2156e81
ExploHash <jbtwaalf@gmail.com>
2021-01-29 16:51:44 +0100

    Test

--- Views/main.php
+++ Views/main.php
@@ -4,4 +4,4 @@
 <p> dit is een work in concept, het dashboard zal in de toekomst verder worden uitgebouwd </p>
 
 
-<h3> momenteel is alleen de cijferpagina functioneel</h3>
+<h3> momenteel is alleen de cijferpagina functioneel :)</h3>
