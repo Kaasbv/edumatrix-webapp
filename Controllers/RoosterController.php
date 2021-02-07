d58201d00b0f6e227ac5bf852984c263e175c523
Wasbeer96 <auke.aardoom@gmail.com>
2021-02-07 14:38:04 +0100

    RoosterController bij

--- Controllers/RoosterController.php
+++ Controllers/RoosterController.php
@@ -1,7 +1,7 @@
 <?php
 class RoosterController extends Controller {
     public function actionGetles(){
-        
+        $les = LesModel::getOne(["id" => $_GET["lesId"]]);
+        $this->renderView("Rooster/lesoverzicht", ["les" => $les]);
     }
 }
-?>
