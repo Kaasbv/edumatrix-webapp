be9a0ca5a98d7b11b55878c71f4432252ecac12e
steef96 <steef96.sb@gmail.com>
2021-03-14 16:22:29 +0100

    jimmy help plz

--- Views/rooster/roosterweergave.php
+++ Views/rooster/roosterweergave.php
@@ -4,13 +4,15 @@
 <?php
 $lessen = [
   (object)[
-    "datumTijd" => "2021-03-04 15:00",
+    "datumTijd" => "2021-03-04 12:00",
+    "duur" => 60,
     "docent" => (object) ["code" => "GEN"],
     "klas" => (object) ["naam" => "HVA1"],
     "vak" => (object) ["naam" => "Frans"]
   ],
   (object)[
-    "datumTijd" => "2021-03-05 16:00",
+    "datumTijd" => "2021-03-05 18:00",
+    "duur" => 120,
     "docent" => (object) ["code" => "Ben"],
     "klas" => (object) ["naam" => "HVA1"],
     "vak" => (object) ["naam" => "Aardrijkskunde"]
@@ -20,40 +22,47 @@ $lessen = [
 ?>
 
 <table id =RoosterWeergave>
-    <tr>
-        <th></th>
-        <?php
-            $startDate = "2021-03-01 14:00";
-            $y = 0;
-            while ($y < 7) {
-                echo "<th>";
-                echo date('D d-M', strtotime($startDate . " + $y days") ) ;
-                echo "</th>";
-            $y++;
-            }
-        ?>
-    </tr>
+    <thead>
+        <tr>
+            <th></th>
+            <?php
+                $startDate = "2021-03-01 14:00";
+                for ($y = 0; $y <7; $y++ ) {
+                    echo "<th class = 'dagWeergave'>";
+                    echo "<div class = 'dag'></div>";
+                    echo date('D d-M', strtotime($startDate . " + $y days") ) ;
+                    echo "</th>";
+                }
+            ?>
+        </tr>
+    </thead>
+    <tbody class = "roosterbody">
     <tr>
         <td class = "RoosterTijd" >00:00 </td>
         <?php
-            $y = 0;
-            while ($y < 7) {
+            for( $y = 0; $y < 7; $y++) {
                 $date = date('d-m-y', strtotime($startDate . " + $y days") );
-                echo "<td rowspan=25>";
+                echo "<td class = 'dagColum' rowspan=25>";
                 foreach ($lessen as $les) {
                     $dateLesson = date('d-m-y', strtotime($les->datumTijd));
                     if($date === $dateLesson){
-                        $hour = date('h', strtotime($les->datumTijd));
-                        echo "<div class='jemoeder' style='margin-top: {$hour}px'>
+                        $hour = date('H', strtotime($les->datumTijd));
+                        $hourCalc = 80 * $hour; 
+                        $minutes  = date ("i" , strtotime($les-> datumTijd));
+                        $minutesCalc = (80/60) * $minutes;
+                        $timeCalc = $hourCalc + $minutesCalc;
+
+                        $lesDuur =  (80/60) * $les ->duur;
+                        echo "<div class='LesInRooster' style='margin-top: {$timeCalc}px; height:{$lesDuur}px'>
                                 <span>{$les->vak->naam}</span>
                                 <span>{$les->klas->naam}</span>
                                 <span>{$les->docent->code}</span>
+                                <span>$hour:$minutes</span>
                             </div>
                         ";
                     }
                 }
                 echo "</td>";
-            $y++;
             }
         ?>
     </tr>
@@ -74,4 +83,5 @@ $lessen = [
             }
         }       
     ?> 
+    </tbody>
 </table>
