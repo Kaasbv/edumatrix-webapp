3c92738ef5b12ece6d8169bda37203b7a2358591
MVAHD <marijn@vanaalst.nu>
2021-03-17 22:22:00 +0100

    RoosterController verder aangepast

--- Controllers/RoosterController.php
+++ Controllers/RoosterController.php
@@ -4,23 +4,30 @@
         private string $beginTijd;
         private string $eindTijd;
         private array $lessen;
-        
+
         public function setTimeWeek(){
             if( isset($_GET["beginTijd"]) ){
-                $this->beginTijd = $_GET["beginTijd"];
+                $dayN = date('N', $_GET["beginTijd"]);
+                $this->beginTijd = date('Y-m-d', strtotime($_GET["beginTijd"] . "- $dayN")) . ' 00:00';
             }else{
-                $this->beginTijd = lower(GETDATE(), week);
+                $this->beginTijd = date('Y-m-d', strtotime("this week")) . ' 00:00';
             }
 
-            $this->eindTijd = upper($this->beginTijd, week);
+            $this->eindTijd = date('Y-m-d', strtotime($this->beginTijd . "+ 1 week")) . ' 23:59';
         }
 
         public function actionIndex(){
-            $this->lessen = Session::$user->getLessen($beginTijd, $eindTijd);
+            $this->setTimeWeek();
+            var_dump($this->beginTijd, $this->eindTijd);
 
-            viewRooster($beginTijd, $eindTijd, $lessen);
+            $this->lessen = Session::$user->getLessen($this->beginTijd, $this->eindTijd);
+
+            //viewRooster($this->beginTijd, $this->eindTijd, $this->lessen);
+        
         }
 
     }
+
 ?>
+
 <p>php:valid</p>
